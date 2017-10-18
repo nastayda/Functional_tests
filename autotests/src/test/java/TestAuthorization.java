08cbbd7b79970861227d1f0baaa47a0c04c35590
nastyada <nastyadanilkina1294@yandex.ru>
2017-10-18 13:28:39 +0300

    Сделала относительные пути для geco, auth-info

---
+++ autotests/src/test/java/TestAuthorization.java
@@ -0,0 +1,88 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Description;
+import ru.yandex.qatools.allure.annotations.Step;
+import ru.yandex.qatools.allure.annotations.Title;
+
+import java.io.File;
+import java.net.Authenticator;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertTrue;
+
+public class TestAuthorization {
+    FirefoxDriver wd;
+    WriteReadFromFile readData;
+    WriteReadFromFile writeData;
+    String resultOfTest;
+
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        //Authenticator.setDefault(new ProxyAuthenticator("ADMDI\\manuhin", "123"));
+        //System.setProperty("http.proxyHost", "proxy.mdi.ru");
+        //System.setProperty("http.proxyPort", "3128");
+
+        //Получить реальный путь к geco
+        File gecoFile = new File("src/help-files/geckodriver.exe");
+        String pathToGeckoDriver = gecoFile.getAbsolutePath();
+        System.out.println(pathToGeckoDriver);
+        //Подготовка утилиты для работы браузера FF
+        System.setProperty("webdriver.gecko.driver", pathToGeckoDriver);
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+        capabilities.setCapability("marionette", true);
+
+        //Получить путь к тестовым файлам
+        File testFile = new File("src/help-files/auth-info.txt");
+
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
+        //Читаем из файла адрес сервера
+        readData= new WriteReadFromFile(testFile.getAbsolutePath());
+        wd.get(readData.readFromFile().get(0).substring(1));
+       // System.out.println(readData.readFromFile().get(0));
+       // wd.get("http://vm-107-stu-dev.ursip.ru/auth/");
+    }
+    @Title("Успешная авторизация пользователя")
+    @Test
+    public void TestAuthorizationNewSuccess() {
+      login("userName", "password", "admin", "admin");
+    }
+
+    private void login(String elementUserName, String elementPassword, String nameLogin, String passwordLogin) {
+        wd.findElement(By.id(elementUserName)).click();
+        wd.findElement(By.id(elementUserName)).clear();
+        wd.findElement(By.id(elementUserName)).sendKeys(nameLogin);
+        wd.findElement(By.id(elementPassword)).click();
+        wd.findElement(By.id(elementPassword)).clear();
+        wd.findElement(By.id(elementPassword)).sendKeys(passwordLogin);
+       // wd.findElement(By.xpath("//div[@class='authorization-page']//button[.='Войти']")).click();
+
+        wd.findElement(By.xpath("//*[@id=\"authorization\"]/div/form/button")).click();
+        //wd.findElement(By.xpath("//*[@id=\"authorization\"]/div/div[2]/div[1]/div/div/span[2]/button1")).isEnabled();
+           // wd.findElement(By.xpath("//*[@id=\"authorization\"]/div/div[2]/div[1]/div/div/span[2]/button")).click();}
+    }
+    @Step("Проверка равенства элементов")
+    public void successfulTest() {
+        assertTrue(21 == 2);
+    }
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
