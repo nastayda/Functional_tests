d24419fdc338318a2bbc09c9e7de5bddbfc11144
nastyada <nastyadanilkina1294@yandex.ru>
2017-11-01 14:27:45 +0300

    Turn back testAuth

--- autotests/src/main/java/test.java
+++ autotests/src/main/java/test.java
@@ -7,14 +7,27 @@ class test {
   public static void main (String[] args) {
     //WriteReadFromFile readData= new WriteReadFromFile("C:\\test\\notes3.txt");
    // System.out.println();
-   try {
+   /*try {
       // Execute command
      //netsh interface set interface name="Подключение по локальной сети" admin=DISABLED
-      String command = "cmd /c start cmd.exe as admin /K runas /profile /user:Administrator /savecred  netsh interface set interface name=\"Подключение по локальной сети\" admin=DISABLED";
+      String command = "cmd /c start cmd.exe /K netsh interface set interface name=\"Подключение по локальной сети\" admin=DISABLED";
       Runtime.getRuntime().exec(command);
     } catch (IOException e) {
-    }
+    }*/
 
+    try {
+      Process p=Runtime.getRuntime().exec("runas /user:Administrator cmd.exe /c start");
+
+      BufferedReader BR=new BufferedReader(new InputStreamReader(p.getInputStream()));
+      String l;
+      while((l=BR.readLine()) != null){
+        System.out.print(l);
+
+      }
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 
 }
